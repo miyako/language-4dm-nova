5eaaf30814b3cc0317d95696429f09412b172dd6
miyako <keisuke.miyako@4D.com>
2026-07-27 09:48:41 +0900

    rm

--- alt-hand-written-grammar/Queries/arguments.scm
+++
@@ -1,7 +0,0 @@
-; The '.' anchor pairs each name with the type that immediately follows it
-; among named siblings, so ($a : Text; $b : Integer) yields two arguments.
-(function_arguments
-  (local_variable_name) @name
-  .
-  (class)? @type
-)
